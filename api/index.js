9c665972caed630af780b857c39f7416e946af08
omoola <omoola410@gmail.com>
2024-07-03 10:16:21 +0100

    dyanamism

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const requestIp = require("request-ip");
 const app = express();
+require("dotenv").config();
+
+
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.get("/", (req, res) => {
   res.send("Hello HNG!! Yor server is running");
@@ -8,17 +12,30 @@ app.get("/", (req, res) => {
 app.get("/api/hello", async (req, res) => {
   const visitor = req.query.visitor_name || "Guest";
   const clientIp = requestIp.getClientIp(req);
+  // const clientIp = "63.116.61.253";
+
+
+ const locationResponse = await fetch(
+   `http://api.weatherapi.com/v1/ip.json?key=${WEATHER_API_KEY}&q=${clientIp}`
+ );
+ const locationData = await locationResponse.json();
+  const { lat, lon, city} = locationData;
+
+ const weatherResponse = await fetch(
+   `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${lat},${lon}`
+ );
+  const weatherData = await weatherResponse.json();
+  console.log(weatherData.current.temp_c);
+
+
+
   res.send({
     client_ip: clientIp,
-    location: "New York",
-    greeting: `Hello, ${visitor}!, the temperature is 11 degrees Celcius in New York`,
+    location: city,
+    greeting: `Hello, ${visitor}!, the temperature is ${weatherData.current.temp_c} degrees Celcius in ${city}`,
   });
 });
 
-app.listen(8000, () => console.log("Server ready on port 3000."));
+app.listen(8000, () => console.log("Server ready on port 8000."));
 
 module.exports = app;
-
-
-
-// http://localhost:3000/api/hello?visitor_name=%22Mark%22
